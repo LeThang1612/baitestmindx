d6b0a2a00ebf80aefbe3df3f7d1c2cb515dae797
LeThang1612 <126088173+LeThang1612@users.noreply.github.com>
2023-04-23 20:32:57 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+function isPrime(n) {
+    if (n < 2) {
+      return false;
+    }
+    for (let i = 2; i <= Math.sqrt(n); i++) {
+      if (n % i === 0) {
+        return false;
+      }
+    }
+    return true;
+  }
+  
+  function calculate() {
+    const a = parseInt(document.getElementById("a").value);
+    const b = parseInt(document.getElementById("b").value);
+  
+    let sum = 0;
+    for (let i = a; i <= b; i++) {
+      if (isPrime(i)) {
+        sum += i;
+      }
+    }
+  
+    document.getElementById("result").innerHTML = `Tổng các số nguyên tố trong khoảng [${a}, ${b}] là ${sum}.`;
+  }
+  
